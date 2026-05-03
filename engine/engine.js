bbb5a31b258cd9ec784e7dcf47d1a7c68e70ce39
momijidaaa <m89184412@gmail.com>
2026-05-03 23:03:52 +0900

    engine.js の更新

--- engine/engine.js
+++ engine/engine.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
-const rules = require('./dictionary.js');
+const rules = require('./dictionary.js'); 
 
-const run = (fileName) => {
-    let code = fs.readFileSync(fileName, 'utf8');
-    
-    // 全ての語録をJSに変換
-    rules.forEach(rule => {
-        code = code.replace(rule.target, rule.replace);
-    });
-
-    code += '\nmain();';
-
+const run = async (fileName) => {
     try {
-        eval(code);
+        let code = fs.readFileSync(fileName, 'utf8');
+        
+        // 辞書に基づいて語録をJSに変換
+        rules.forEach(rule => {
+            code = code.replace(rule.target, rule.replace);
+        });
+
+        // 実行準備
+        code += '\nmain();';
+        let output = [];
+        
+        // 実行用の関数を作成
+        const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
+        const fn = new AsyncFunction('output', code);
+        
+        await fn(output);
+        console.log("\x1b[33m--- 実行結果 ---\x1b[0m");
+        console.log(output.join('\n'));
     } catch (e) {
-        console.log("【エラー】ｱｯｱｯｱｯｱ");
-        console.log(e.message);
+        console.log("\x1b[31m【エラー】ｱｯｱｯｱｯｱ\x1b[0m\n", e.message);
     }
 };
 
