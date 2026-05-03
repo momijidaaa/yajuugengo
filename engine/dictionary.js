b5d57075d0ca86091f3993adcceee012d382cb79
momijidaaa <m89184412@gmail.com>
2026-05-03 23:02:45 +0900

    dictionary.js の更新

--- engine/dictionary.js
+++ engine/dictionary.js
@@ -1,5 +1,5 @@
 module.exports = [
-    // --- 基本 ---
+    // --- 基本（開始・変数・出力・終了） ---
     { name: 'start', target: /24歳、学生です。/g, replace: 'async function main() {' },
     { name: 'var', target: /やりますねぇ！\s+(\w+)\s*=\s*(.+)/g, replace: 'let $1 = $2;' },
     { name: 'print', target: /ブッチッパ！\s*「(.+?)」/g, replace: 'output.push("$1");' },
@@ -10,10 +10,12 @@ module.exports = [
     { name: 'num1', target: /いいよ！来いよ！/g, replace: '114514' },
     { name: 'num2', target: /イクイク/g, replace: '1919' },
 
-    // --- 制御・時間 ---
+    // --- 制御・時間・エラー ---
     { name: 'wait', target: /おまたせ！アイスティーしかなかったけど、いいかな？\s*(\d+)/g, replace: 'await new Promise(r => setTimeout(r, $1));' },
     { name: 'error', target: /ンアッー！/g, replace: 'throw new Error("（迫真）");' },
-    
+    { name: 'try', target: /✝悔い改めて✝/g, replace: 'try {' },
+    { name: 'catch', target: /これもうわかんねぇなぁ。/g, replace: '} catch (e) {' },
+
     // --- 比較 ---
     { name: 'if', target: /アアーッ！\s*\((.+)\)/g, replace: 'if ($1) {' },
     { name: 'else', target: /ぬわあああん！/g, replace: '} else {' }
