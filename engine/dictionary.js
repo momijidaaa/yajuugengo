cdcc4c54b2824581afe94f922c4d2deb4826d133
momijidaaa <m89184412@gmail.com>
2026-05-03 23:00:25 +0900

    dictionary.js の更新

--- engine/dictionary.js
+++ engine/dictionary.js
@@ -1,8 +1,20 @@
 module.exports = [
+    // --- 基本 ---
     { name: 'start', target: /24歳、学生です。/g, replace: 'async function main() {' },
     { name: 'var', target: /やりますねぇ！\s+(\w+)\s*=\s*(.+)/g, replace: 'let $1 = $2;' },
-    { name: 'print', target: /ブッチッパ！\s*「(.+?)」/g, replace: 'console.log("$1");' },
+    { name: 'print', target: /ブッチッパ！\s*「(.+?)」/g, replace: 'output.push("$1");' },
+    { name: 'end', target: /王道を往く/g, replace: '}' },
+
+    // --- 計算・定数 ---
     { name: 'add', target: /菅野美穂/g, replace: '+' },
+    { name: 'num1', target: /いいよ！来いよ！/g, replace: '114514' },
+    { name: 'num2', target: /イクイク/g, replace: '1919' },
+
+    // --- 制御・時間 ---
+    { name: 'wait', target: /おまたせ！アイスティーしかなかったけど、いいかな？\s*(\d+)/g, replace: 'await new Promise(r => setTimeout(r, $1));' },
+    { name: 'error', target: /ンアッー！/g, replace: 'throw new Error("（迫真）");' },
+    
+    // --- 比較 ---
     { name: 'if', target: /アアーッ！\s*\((.+)\)/g, replace: 'if ($1) {' },
-    { name: 'end', target: /王道を往く/g, replace: '}' }, // ブロックの終わり
+    { name: 'else', target: /ぬわあああん！/g, replace: '} else {' }
 ];
